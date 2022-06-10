f559a8c5e09da14f4062b00211103653ff074cea
Sarthak Agrawal <sarthak.agrawal@healthplix.com>
2022-06-10 15:37:11 +0530

    Learn slice byte

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 
-	cards := newDeck ()
+	// cards := newDeck ()
 
-	hand, remainingCards := deal (cards, 5)
+	// hand, remainingCards := deal (cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+
+	greetings := "Hi there!"
+	fmt.Println ([]byte ( greetings))
 }
 
