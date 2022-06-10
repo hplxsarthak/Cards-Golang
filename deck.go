53c0c7f0050ddf74fe414b86694a36a93be96daf
Sarthak Agrawal <sarthak.agrawal@healthplix.com>
2022-06-10 19:06:39 +0530

    Optimize shuffle function for every time random generation by changing seed using time package

--- deck.go
+++ deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -65,9 +66,21 @@ func newDeckFromFile (filename string) deck {
 	return ans;
 }
 
+// func (d deck) shuffle () {
+// 	for i := range d {
+// 		newPosition := rand.Intn(len(d) -1 )
+
+// 		d[i],d[newPosition] = d[newPosition], d[i]
+// 	}
+// }
+
 func (d deck) shuffle () {
+	source := rand.NewSource(time.Now().UnixNano())
+
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) -1 )
+		newPosition := r.Intn(len(d) -1 )
 
 		d[i],d[newPosition] = d[newPosition], d[i]
 	}
