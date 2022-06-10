7610c87c15f0c97f9dbb8e618cc58e6853560be7
Sarthak Agrawal <sarthak.agrawal@healthplix.com>
2022-06-10 13:34:56 +0530

    Add a new Deck function which consist of pack of cards

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 func main() {
-
-	// cards := [] string {"Ace of spades", newCard()}
-
-	cards := deck{"Ace of spades", newCard()}
-
-	cards = append(cards, "Six of hearts")
+	
+	cards := newDeck ()
 
 	cards.print()
 }
 
-func newCard() string {
-	return "Five of Diamonds"
-}
